dc087ea5d0f96c5d0fe648696c66005f44c0fdb9
Enguerrand Crouzet <enguerrand.crouzet@epfedu.fr>
2023-10-12 20:03:09 +0200

    tp2 Crouzet Les meilleurs brasseries

---
+++ TP2_Bieres_Crouzet/src/tp2_bieres_crouzet/BouteilleBiere.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_crouzet;
+
+/**
+ *
+ * @author engue
+ */
+public class BouteilleBiere {
+
+    double degreAlcool;
+
+    String Nom;
+
+    String brasserie;
+
+    boolean ouverte; 
+
+ 
+
+public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+
+Nom = unNom;
+
+degreAlcool = unDegre;
+
+brasserie = uneBrasserie;
+
+ouverte = false;
+
+}
+
+ 
+
+public void lireEtiquette() {
+
+ 
+
+System.out.println("Bouteille de " + Nom +" (" + degreAlcool +
+
+" degres) \nBrasserie : " + brasserie ) ;
+
+}
+
+ 
+
+public void Décapsuler() {
+
+ 
+
+if (ouverte == false) {
+
+    ouverte = true;
+
+    System.out.println("La bière est maintenant ouverte");
+
+} else if (ouverte == true) {
+
+    System.out.println("La bière est déjà ouverte");
+
+}
+
+ 
+
+}
+
+ 
+
+@Override
+
+public String toString() {
+
+String chaine_a_retourner;
+
+chaine_a_retourner = Nom + " (" + degreAlcool + " degrés) ouverte ? ";
+
+if (ouverte == true )
+
+    chaine_a_retourner += "oui" ;
+
+else chaine_a_retourner += "non" ;
+
+    return chaine_a_retourner ;
+
+}
+
+}
+    
---
+++ TP2_Bieres_Crouzet/src/tp2_bieres_crouzet/TP2_Bieres_Crouzet.java
@@ -0,0 +1,89 @@
+/*
+ * C
+ */
+package tp2_bieres_crouzet;
+
+/**
+ *
+ * @author engue
+ */
+public class TP2_Bieres_Crouzet {
+ 
+
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+
+   
+
+    BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls",7.0 ,"Dubuisson") ;
+
+ 
+
+    uneBiere.lireEtiquette();
+
+    uneBiere.Décapsuler();
+
+    System.out.println(uneBiere) ;
+
+   
+
+    BouteilleBiere deuxBiere = new BouteilleBiere("Leffe",6.6 ,"Abbaye de Leffe") ;
+
+     
+
+    deuxBiere.lireEtiquette();
+
+    deuxBiere.Décapsuler();
+
+    System.out.println(deuxBiere) ;
+
+    
+
+    BouteilleBiere troisBiere = new BouteilleBiere("Heineken",5.0 ,"VieuxTabac") ;
+
+   
+
+        troisBiere.lireEtiquette();
+
+        troisBiere.Décapsuler();
+
+        System.out.println(troisBiere) ;
+
+       
+
+    BouteilleBiere quatreBiere = new BouteilleBiere("Licorne",6.0 ,"Novelty") ;
+
+   
+
+        quatreBiere.lireEtiquette();
+
+        quatreBiere.Décapsuler();
+
+        System.out.println(quatreBiere) ;
+
+   
+
+    BouteilleBiere cinqBiere = new BouteilleBiere("Rince cochon",6.0 ,"3brasseurs") ;
+
+   
+
+        cinqBiere.lireEtiquette();
+
+        cinqBiere.Décapsuler();
+
+        System.out.println(cinqBiere) ;
+
+  
+
+    }
+
+     
+
+}
