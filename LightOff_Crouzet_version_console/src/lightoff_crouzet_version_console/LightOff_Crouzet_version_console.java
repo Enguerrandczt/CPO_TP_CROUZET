94c53c227c5870c3963eed8f401c6f46a7f61679
Enguerrand Crouzet <enguerrand.crouzet@epfedu.fr>
2023-10-25 12:49:21 +0200

    Projet guidé 1 er commit

---
+++ LightOff_Crouzet_version_console/src/lightoff_crouzet_version_console/LightOff_Crouzet_version_console.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_crouzet_version_console;
+
+/**
+ *
+ * @author engue
+ */
+public class LightOff_Crouzet_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CelluleLumineuse cellule1 = new CelluleLumineuse();
+        System.out.print(cellule1);
+        CelluleLumineuse cellule2 = new CelluleLumineuse();
+        System.out.print(cellule2);
+        
+        cellule1.activerCellule();
+        System.out.println(cellule1);
+             
+                
+    }
+    
+}
